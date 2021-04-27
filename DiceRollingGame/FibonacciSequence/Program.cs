25d85d7fdb5e1711e7cc31b56a4954059c5ad051
Omer Citik <omer.citik@hva.nl>
2021-04-27 22:25:25 +0200

    FibonacciSequence

---
+++ DiceRollingGame/FibonacciSequence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSequence {
+    class Program {
+        static void Main(string[] args) {
+
+            var fibonacciSequence = new List<int> { 1, 1 };
+
+            while (fibonacciSequence.Count<20) {
+                // Get last 2 numbers
+                int lastNum = fibonacciSequence[fibonacciSequence.Count-1];
+                int secondNum = fibonacciSequence[fibonacciSequence.Count-2];
+
+                fibonacciSequence.Add(lastNum+secondNum);
+            }
+
+            foreach (int num in fibonacciSequence) {
+                    Console.WriteLine(num);
+             }
+
+        }
+    }
+}
